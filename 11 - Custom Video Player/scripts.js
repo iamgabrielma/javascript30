1df4d2d53a313b8303331de9141c798c9935d3db
Gabriel Maldonado Almendra <gma992@gabriels-macbook-pro-2.home>
2017-06-24 11:18:15 +0200

    updates range for volume and speed + updates video progress bar on play

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,6 +2,8 @@ const player = document.querySelector('.player');
 const video = document.querySelector('.viewer');
 const toggle = document.querySelector('.toggle');
 const skipButtons = document.querySelector('[data-skip]');
+const ranges = document.querySelector('.player__slider');
+const progressBar = document.querySelector('.progress__filled');
 
 // Play-Pause on click
 function togglePlay(){
@@ -19,16 +21,33 @@ function updateButton(){
 	toggle.textContent = icon;
 }
 
-//
+// Jumps video back and forward on skipButtons click
 function skip(){
 	//console.log('Clicking some [data-skip]!');
 	console.log(this.dataset);
+	// Jump video back and forward (!) + string to float
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Updates range for volume and speed
+function handleRangeUpdate(){
+	//console.log("range update!" + parseFloat(this.dataset));
+	console.log(this.name + " range update! " + parseFloat(ranges.value));
+}
+
+// Updates video progress bar
+function handleProgress(){
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`
+	;
+	//console.log('calling on time progress!');
 }
 
 /* Event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 
@@ -42,3 +61,8 @@ skipButtons.addEventListener('click', skip);
 // console.log(skipButtons);
 // const newButtons =  Array.prototype.skipButtons;
 // console.log(newButtons);
+
+// FOREACH IS NOT A FUNCTION ERROR:
+//ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.addEventListener('change', handleRangeUpdate);
+ranges.addEventListener('mousemove', handleRangeUpdate);
